fca8f1fa688a8b309bbc07c175cb3a16ce25651b
liorwen <zear19st@gmail.com>
2017-02-15 14:01:12 +0800

    Rebuild

--- src/Main.ts
+++ src/Main.ts
@@ -45,7 +45,7 @@ class Main extends egret.DisplayObjectContainer {
         //Config to load process interface
         this.loadingView = new LoadingUI();
         this.stage.addChild(this.loadingView);
-        this.stage.scaleMode = egret.StageScaleMode.NO_BORDER;
+        this.stage.scaleMode = egret.StageScaleMode.EXACT_FIT;
         this.stage.orientation = egret.OrientationMode.PORTRAIT;
         //初始化Resource资源加载库
         //initiate Resource loading library
